d541b0eef796d2bf4d125589a9fef08558e587de
KrishanVeer Singh <krishanveerdata@gmail.com>
2025-09-25 11:23:17 +0530

    exp

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -8,37 +8,43 @@ import gsap from "gsap";
 const Experience = () => {
   useGSAP(() => {
     // Education section
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: "#experience", start: "top 80%"
-      }
-    })
-      .fromTo("#experience .exp-item",
-      { opacity: 0, y: 80 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 0.9,
-        ease: "power1.inOut"
-        
-      }
-    )
-    .fromTo("#extitle",
-      { opacity: 0, yPercent: 50 },
-      {
-        
-        opacity: 1,
-        yPercent: 0,
-        duration: 0.7,
-        ease: "power1.inOut",
-      }
-    )
-  }
-)
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#experience",
+          start: "top 80%",
+        },
+      })
+      .fromTo(
+        "#experience .exp-item",
+        { opacity: 0, y: 80 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.9,
+          ease: "power1.inOut",
+        }
+      )
+      .fromTo(
+        "#extitle",
+        { opacity: 0, yPercent: 50 },
+        {
+          opacity: 1,
+          yPercent: 0,
+          duration: 0.7,
+          ease: "power1.inOut",
+        }
+      );
+  });
   return (
-    <div className="!max-w-4xl mx-auto mt-[10rem] lg:pl-0 pl-4 lg:pr-0 pr-3" id="experience">
-      <h1 className="font-semibold text-3xl" id='extitle'>Experience</h1>
-      <div className=" exp-item border-l border-gray-500 pl-4 mt-8" >
+    <div
+      className="!max-w-4xl mx-auto mt-[10rem] lg:pl-0 pl-4 lg:pr-0 pr-3"
+      id="experience"
+    >
+      <h1 className="font-semibold text-3xl" id="extitle">
+        Experience
+      </h1>
+      <div className=" exp-item border-l border-gray-500 pl-4 mt-8">
         <div className="flex items-center gap-1 mt-2 text-gray-400 text-lg pl-1">
           <figure>
             <MdLocationOn className="size-4 " />
@@ -46,25 +52,55 @@ const Experience = () => {
           <figcaption className="font-sans">Delhi, India</figcaption>
         </div>
         <div className="flex justify-between items-center">
-          <h1 className="lg:text-3xl text-2xl mt-2 pl-2 font-semibold">Edulty India</h1>
-          <p className="font-semibold text-md">SEP 2024 – DEC 2024</p>
+          <h1 className="lg:text-3xl text-2xl mt-2 pl-2 font-semibold">
+            Edulty India
+          </h1>
+          <p className="font-semibold text-md">JAN 2024 – MAY 2024</p>
         </div>
         <p className="pl-3 mt-2 font-semibold text-gray-400 lg:text-lg">
           Web Developer Intern
         </p>
         <div className="pl-3 mt-4 ">
-          <Expt desc='Crafted responsive React components with Tailwind CSS for
-            educational web pages, enhancing user experience.'/>
-                  
-            <Expt desc='Assisted in building and testing Node.js/Express APIs, contributing
-            to backend functionality.'/>
-                  
-             <Expt desc='Collaborated with the team using Git & GitHub, streamlining version
-            control and workflow.'/>
-                  
-            <Expt desc='Improved coding efficiency through debugging, problem-solving, and
-            deploying small projects in a real-world environment.'/>
-                  
+          <Expt
+            desc="Crafted responsive React components with Tailwind CSS for
+            educational web pages, enhancing user experience."
+          />
+
+          <Expt
+            desc="Assisted in building and testing Node.js/Express APIs, contributing
+            to backend functionality."
+          />
+
+          <Expt
+            desc="Collaborated with the team using Git & GitHub, streamlining version
+            control and workflow."
+          />
+
+          <Expt
+            desc="Improved coding efficiency through debugging, problem-solving, and
+            deploying small projects in a real-world environment."
+          />
+        </div>
+
+        <div className="flex items-center gap-1 mt-15 text-gray-400 text-lg pl-1">
+          <figure>
+            <MdLocationOn className="size-4 " />
+          </figure>
+          <figcaption className="font-sans">Noida, India</figcaption>
+        </div>
+        <div className="flex justify-between items-center">
+          <h1 className="lg:text-3xl text-2xl mt-2 pl-2 font-semibold">
+            Philpay
+          </h1>
+          <p className="font-semibold text-md">SEP 2024 – SEP 2025</p>
+        </div>
+        <p className="pl-3 mt-2 font-semibold text-gray-400 lg:text-lg">
+          MIS Analyst
+        </p>
+        <div className="pl-3 mt-4 ">
+          <Expt desc="Automated repetitive reporting tasks using Google Apps Script (JavaScript-based) and VBA, reducing manual effort by 40%." />
+          <Expt desc="Designed and maintained data pipelines between Google Sheets, SQL, and Power BI for real-time business reporting." />
+          <Expt desc="Collaborated with cross-functional teams to analyze business requirements and develop custom reporting solutions." />
         </div>
       </div>
     </div>
--- src/component/Intro.jsx
+++ src/component/Intro.jsx
@@ -9,31 +9,36 @@ import gsap from "gsap";
 
 const Intro = () => {
   useGSAP(() => {
-    gsap.fromTo("#profile-pic",
+    gsap.fromTo(
+      "#profile-pic",
       { scale: 0.8, opacity: 0 },
-      { scale: 1, opacity: 1, duration: 1, ease: "back.out(1.7)" })
-    gsap.fromTo("#intro-text",
+      { scale: 1, opacity: 1, duration: 1, ease: "back.out(1.7)" }
+    );
+    gsap.fromTo(
+      "#intro-text",
       { y: 50, opacity: 0 },
       { y: 0, opacity: 1, duration: 1, ease: "power2.out", stagger: 0.2 }
     );
-    gsap.fromTo("#social-links",
-  { y: 20, opacity: 0 },
-  { y: 0, opacity: 1, duration: 0.8, stagger: 0.2 }
-);
-
-
-})
+    gsap.fromTo(
+      "#social-links",
+      { y: 20, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.8, stagger: 0.2 }
+    );
+  });
   return (
     <div id="intro !max-w-4xl">
       <div className="flex flex-col lg:flex-row lg:justify-between items-center mt-8 pt-6  mx-auto gap-20 border-b border-gray-600 pb-8">
-        <div className="flex lg:flex-row flex-col gap-10 justify-center items-center"  >
+        <div className="flex lg:flex-row flex-col gap-10 justify-center items-center">
           <img
             id="profile-pic"
             src="Profile.jpg"
             alt="prof"
             className="lg:size-40 rounded-xl h-45 w-50"
           />
-          <div className="flex items-center flex-col justify-center lg:items-start "  id='intro-text'>
+          <div
+            className="flex items-center flex-col justify-center lg:items-start "
+            id="intro-text"
+          >
             <h1 className="text-3xl font-bold">Krishanveer Singh</h1>
             <h2 className="text-2xl mt-2 font-semibold text-gray-400">
               Web Developer
@@ -57,8 +62,8 @@ const Intro = () => {
         </div>
 
         <a
-            href="RESUME_WebDev.pdf"
-            download='Krishanveer_CV.pdf'
+          href="Resume_WebDev.pdf"
+          download="Krishanveer_CV.pdf"
           className="flex items-center gap-1 border-b relative z-10 border-gray-500 pb-1 hover:border-white transition-colors duration-300 hover:shadow hover:scale-110 lg:text-xl text-lg"
         >
           <span>
@@ -68,9 +73,12 @@ const Intro = () => {
         </a>
       </div>
 
-      <div className="!max-w-5xl mx-auto lg:justify-between lg:flex-row flex flex-col justify-center items-center" id='social-links' >
+      <div
+        className="!max-w-5xl mx-auto lg:justify-between lg:flex-row flex flex-col justify-center items-center"
+        id="social-links"
+      >
         <CopyEmail />
-        <div className="flex items-center gap-3" >
+        <div className="flex items-center gap-3">
           <a
             href="https://github.com/KvLetsCode"
             target="_blank"
